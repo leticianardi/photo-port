8c70b24eb9b04dff9c994ae400fa5a90a17c28bc
leticianardi <leticia.tnardi@gmail.com>
2022-02-12 22:47:06 -0800

    deployng

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -9,6 +9,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -17,6 +19,8 @@ it('renders', () => {
     categories={categories}
     setCurrentCategory={mockSetCurrentCategory}
     currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
   />);
 })
 
